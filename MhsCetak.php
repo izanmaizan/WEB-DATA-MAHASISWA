c3d385a59bf56853c91de022ed7385ac3cdf62b2
izanmaizan <izanmaizan@gmail.com>
2023-08-04 22:52:27 +0700

    udah nyerah segitu aja

--- MhsCetak.php
+++ MhsCetak.php
@@ -10,9 +10,9 @@ while ($row = mysqli_fetch_array($sql))
 }
 $judul = "Data Mahasiswa";
 $header = array(
-    array("label" => "NIM", "length" => 25, "align" => "L"),
-    array("label" => "Nama Mahasiswa", "length" => 40, "align" => "L"),
-    array("label" => "UMUR", "length" => 15, "align" => "L"),
+    array("label" => "NIM", "length" => 30, "align" => "L"),
+    array("label" => "Nama Mahasiswa", "length" => 60, "align" => "L"),
+    array("label" => "UMUR", "length" => 30, "align" => "L"),
     array("label" => "JENIS KELAMIN", "length" => 30, "align" => "L")
 );
 
@@ -26,25 +26,27 @@ $pdf->SetFillColor(255, 0, 0);
 $pdf->SetTextColor(225);
 $pdf->SetDrawColor(128, 0, 0);
 
-foreach ($header as $kolom)
-{
-    $pdf->Cell($kolom['length'], 10, $kolom['label'], 1, 0, $kolom['align'], true);
-}
-$pdf->Ln();
+    foreach ($header as $kolom)
+    {
+        $pdf->Cell($kolom['length'], 5, $kolom['label'], 1, 0, $kolom['align'], true);
+    }
+    $pdf->Ln();
 
-$pdf->SetFillColor(244, 235, 255);
-$pdf->SetTextColor(0);
-$pdf->SetFont('Arial', '', 10);
-$fill = false;
+    $pdf->SetFillColor(244, 235, 255);
+    $pdf->SetTextColor(0);
+    $pdf->SetFont('Arial', '', 10);
+    $fill = false;
+
 
 foreach ($data as $baris)
 {
+    $fill = !$fill;
+    $i = 0;
     foreach ($baris as $cell)
     {
-        $pdf->Cell($header[$i]['length'], 10, $cell, 1, 0, $header[$i]['align'], $fill);
+        $pdf->Cell($kolom['length'], 5, $cell, 1, 0, $kolom['align'], $fill);
         $i++;
     }
-    $fill = !$fill;
     $pdf->Ln();
 }
 
