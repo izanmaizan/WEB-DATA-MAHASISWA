393ef7d280f5e355d3eef1920c5a9f44e6f1a7ff
izanmaizan <izanmaizan@gmail.com>
2023-08-04 09:14:28 +0700

    memperbaiki proses_cari.php yang bermasalah

--- proses_cari.php
+++ proses_cari.php
@@ -1,65 +1,61 @@
 <?php
-
-    $connect = mysqli_connect("localhost", "root", "");
-    mysqli_select_db($connect, "akademik");
+    $conn = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($conn, "akademik");
     $bagianWhere = "";
-    if(isset($_POST['nimCat']))
+
+    if(isset($_POST['nimCat']) && !empty($_POST['NIM']))
     {
-        $NIM = $_POST['NIM'];
-        if(empty($bagianWhere))
-        {
-            $bagianWhere .= "NIM = '$NIM'";
-        }
+        $NIM = mysqli_real_escape_string($conn, $_POST['NIM']);
+        $bagianWhere .= "NIM = '$NIM'";
     }
 
-    if(isset($_POST['namaCat']))
+    if(isset($_POST['namaCat']) && !empty($_POST['Nama']))
     {
-        $nama = $_POST['Nama'];
-        if(empty($bagianWhere))
+        $nama = mysqli_real_escape_string($conn, $_POST['Nama']);
+        if(!empty($bagianWhere))
         {
-            $bagianWhere .= " AND Nama LIKE '%$nama%'";
+            $bagianWhere .= " AND ";
         }
-        else
-        {
-            $bagianWhere .= " AND Nama LIKE '%$nama%'";
-        }
+        $bagianWhere .= "Nama LIKE '%$nama%'";
     }
     
-    if(isset($_POST['umurCat']))
+    if(isset($_POST['umurCat']) && !empty($_POST['Umur']))
     {
-        $umur = $_POST['Umur'];
-        if(empty($bagianWhere))
+        $umur = mysqli_real_escape_string($conn, $_POST['Umur']);
+        if(!empty($bagianWhere))
         {
-            $bagianWhere .= " AND Umur LIKE '%$umur%'";
+            $bagianWhere .= " AND ";
         }
-        else
-        {
-            $bagianWhere .= " AND Umur LIKE '%$umur%'";
-        }
+        $bagianWhere .= "Umur LIKE '%$umur%'";
     }
     
-    if(isset($_POST['SEXCat']))
+    if(isset($_POST['SEXCat']) && !empty($_POST['SEX']))
     {
-        $SEX = $_POST['SEX'];
-        if(empty($bagianWhere))
+        $SEX = mysqli_real_escape_string($conn, $_POST['SEX']);
+        if(!empty($bagianWhere))
         {
-            $bagianWhere .= " AND SEX LIKE '%$SEX%'";
+            $bagianWhere .= " AND ";
         }
-        else
-        {
-            $bagianWhere .= " AND SEX LIKE '%$SEX%'";
-        }
+        $bagianWhere .= "SEX LIKE '%$SEX%'";
     }
     
-    
-    $query = "SELECT * FROM mahasiswa WHERE " . $bagianWhere;
-    $hasil = mysqli_query($connect, $query);
+    $query = "SELECT * FROM mahasiswa";
+    if (!empty($bagianWhere)) {
+        $query .= " WHERE " . $bagianWhere;
+    }
+
+    $hasil = mysqli_query($conn, $query);
+
+    if (!$hasil) {
+        die("Query error: " . mysqli_error($conn));
+    }
+
     echo "<table border='1'>";
     echo "<tr><td>NIM</td><td>Nama Mahasiswa</td><td>Umur</td><td>Jenis Kelamin</td></tr>";
     while($data = mysqli_fetch_array($hasil, MYSQLI_ASSOC))
     {
         echo
-        "<tr><td>NIM</td><td>" . $data['Nama'] . "</td><td>" . $data['Umur'] . "</td><td>" . $data['SEX'] . "</td></td>";
+        "<tr><td>" . $data['NIM'] . "</td><td>" . $data['Nama'] . "</td><td>" . $data['Umur'] . "</td><td>" . $data['SEX'] . "</td></tr>";
     }
     echo "</table>";
 ?>
