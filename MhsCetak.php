eef80ec8ac59a81394cd44b77b807be681e56563
izanmaizan <izanmaizan@gmail.com>
2023-08-04 20:09:12 +0700

    memperbaiki eror

--- MhsCetak.php
+++ MhsCetak.php
@@ -1,42 +1,53 @@
 <?php
-    require_once ("connect.php");
-    $NIM = $_POST['txtnim'];
-    $query = "SELECT * FROM mahasiswa WHERE NIM = '$NIM'";
-    $sql = mysqli_query($connect, $query);
-    $data = array();
-    while ($row = mysqli_fetch_array($sql))
-    {
-        array_push($data, $row);
-    }
-    $judul = "";
-    $header = array(array("label" => "NIM", "length" =>25, "align" => "L"), array("label" => "Nama Mahasiswa", "length" => 40, "align" => "L"), array("label" => "UMUR", "length" =>15, "align" => "L"), array("label" => "JENIS KELAMIN", "length" =>30, "align" => "L"));
+require_once("connect.php");
+$NIM = $_POST['txtnim'];
+$query = "SELECT * FROM mahasiswa WHERE NIM = '$NIM'";
+$sql = mysqli_query($connect, $query);
+$data = array();
+while ($row = mysqli_fetch_array($sql))
+{
+    array_push($data, $row);
+}
+$judul = "Data Mahasiswa";
+$header = array(
+    array("label" => "NIM", "length" => 25, "align" => "L"),
+    array("label" => "Nama Mahasiswa", "length" => 40, "align" => "L"),
+    array("label" => "UMUR", "length" => 15, "align" => "L"),
+    array("label" => "JENIS KELAMIN", "length" => 30, "align" => "L")
+);
 
-    require ("fpdf16/fpdf.php");
-    $pdf = new FPDF();
-    $pdf -> AddPage();
-    $pdf -> SetFont("Arial", 'B', 0);
-    $pdf -> Cell(0, 8, $judul, '0', 1, 'C');
-    $pdf -> SetFont('Arial', '', 10);
-    $pdf -> SetFillColor(255, 0, 0);
-    $pdf -> SetTextColor(225);
-    $pdf -> SetDrawColor(128, 0, 0);
-    foreach ($header as $kolom)
+require ("fpdf16/fpdf.php");
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont("Arial", 'B', 16);
+$pdf->Cell(0, 10, $judul, 0, 1, 'C');
+$pdf->SetFont('Arial', '', 10);
+$pdf->SetFillColor(255, 0, 0);
+$pdf->SetTextColor(225);
+$pdf->SetDrawColor(128, 0, 0);
+
+foreach ($header as $kolom)
+{
+    $pdf->Cell($kolom['length'], 10, $kolom['label'], 1, 0, $kolom['align'], true);
+}
+$pdf->Ln();
+
+$pdf->SetFillColor(244, 235, 255);
+$pdf->SetTextColor(0);
+$pdf->SetFont('Arial', '', 10);
+$fill = false;
+
+foreach ($data as $baris)
+{
+    $i = 0;
+    foreach ($baris as $cell)
     {
-        $pdf -> Cell($kolom['length'], 5, $kolom['label'], 1, '0', $kolom['align'], true);
+        $pdf->Cell($header[$i]['length'], 10, $cell, 1, 0, $header[$i]['align'], $fill);
+        $i++;
     }
+    $fill = !$fill;
     $pdf->Ln();
-    $pdf->SetFillColor(244,235,255);
-    $pdf->SettextColor(0);
-    $pdf->SetFont('arial','','10');
-    $fill =false;
-    foreach ($Data as $Baris){
-        $i= 0;
-        foreach ($Baris as $Cell){
-            $pdf->Cell ($Header[$i]['length'], 7, $Cell, 2, '0', $Kolom['align'], $fill);
-            $i++;
-        }
-        $fill = !$fill;
-        $pdf->Ln();
-    }
-    $pdf->Output();
+}
+
+$pdf->Output();
 ?>
