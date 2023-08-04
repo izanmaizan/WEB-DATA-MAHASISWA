e74a9c3ef0e8fdb04202d866688381d9a1689a0e
izanmaizan <izanmaizan@gmail.com>
2023-08-04 10:11:53 +0700

    menambahkan MhsLaporan.php

---
+++ MhsLaporan.php
@@ -0,0 +1,32 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laporan Mahasiswa</title>
+</head>
+
+<body>
+    <form action="post" action="MhsCetak.php" name="form1" target="_self">
+        <table border="0" width="540" cellspacing="1" cellpadding="2">
+            <tr>
+                <td colspan="2">
+                    <div align="center">
+                        MASUKKAN DATA NIM
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <label for="">
+                        <input type="submit" value="Cetak" name="Submit">
+                        <input type="reset" value="Batal" name="Submit2">
+                    </label>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
