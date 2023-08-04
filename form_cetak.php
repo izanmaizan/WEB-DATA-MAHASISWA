6b72bdee2a044a88d1816214c127ab5ab2b55d15
izanmaizan <izanmaizan@gmail.com>
2023-08-04 09:45:51 +0700

    menambahkan file form_cetak.php

---
+++ form_cetak.php
@@ -0,0 +1,25 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Laporan Mahasiswa</title>
+</head>
+
+<body>
+    <h1>Mencetak Data Mahasiswa</h1>
+    <form action="MhsCetak.php" method="get">
+        <table>
+            <tr>
+                <td>NIM</td>
+                <td><input type="text" name="txtnim" id=""></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="Submit" name="submit"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
